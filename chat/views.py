fa3f62b5203fdbdd86a43f773af95f3e7217a9a9
jmukes97 <jmukes@oakland.edu>
2019-04-03 16:57:55 -0400

    Added security over the chat; chat will display username

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 import json
+from webapp.models import user, SearchCriteria
+
+
 
 def index(request):
     return render(request, 'chat/index.html', {})
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-    'room_name_json': mark_safe(json.dumps(room_name))
-})
+    if not 'email' in request.session:
+        return redirect("/")
+    email = request.session['email']
+    try:
+        userObj = user.objects.get(pk=email)
+    except user.DoesNotExist:
+        return redirect("/")
+
+    username = userObj.username
+    return render(request, 'chat/room.html', {'room_name_json': mark_safe(json.dumps(room_name)), "user":username })
